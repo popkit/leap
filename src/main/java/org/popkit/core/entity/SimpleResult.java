da32cc6b922f65a8177e8746c9f27b789b6d6ac9
蒋国宝 <loveaborn@foxmail.com>
2016-05-16 11:49:26 +0800

    add simplereulst

---
+++ src/main/java/org/popkit/core/entity/SimpleResult.java
@@ -0,0 +1,42 @@
+package org.popkit.core.entity;
+
+/**
+ * Created by Aborn Jiang
+ * Mail aborn.jiang@gmail.com
+ * 2016-05-16:11:47
+ */
+public class SimpleResult {
+    private boolean success;
+    private String info;
+
+    public SimpleResult() {
+        this.success = false;
+        this.info = "";
+    }
+
+    public SimpleResult(boolean success, String info) {
+        this.success = success;
+        this.info = info;
+    }
+
+    public void update(boolean status, String info) {
+        this.success = status;
+        this.info = info;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
